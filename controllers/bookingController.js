5abe19092ec449837f34aef68114dde6495146b9
akro999 <akarrouch27011@gmail.com>
2023-02-04 21:59:10 +0100

    fix stripe 1

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,8 +55,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // };
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const user = (await User.findOne({ email: session.email }))._id;
+  const price = session.amount_total / 100;
   await Booking.create({ user, tour, price });
 };
 exports.webhookCheckout = (req, res, next) => {
