bbf609c334a61385322ce02e23800f2c8dca6f47
akro999 <akarrouch27011@gmail.com>
2023-02-04 23:13:20 +0100

    adding alert 1

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -16,6 +16,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
+    // success_url: `${req.protocol}://${req.get(
+    //   'host'
+    // )}/my-bookings?alert=booking`,
     success_url: `${req.protocol}://${req.get(
       'host'
     )}/my-bookings?alert=booking`,
