1266760e0bb499fca921cc0d649268b883935a1b
akro999 <akarrouch27011@gmail.com>
2023-02-04 22:17:25 +0100

    stripe 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,22 +55,23 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // };
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.email }))._id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
-  await Booking.create({ user, tour, price });
+  await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers['stripe-signature'];
+  const sig = req.headers['stripe-signature'];
   let event;
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
+      sig,
+      process.env.WEBHOOK_SECRET
     );
   } catch (err) {
-    return res.status(400).send(`Webhook error : ${err.message}`);
+    return res.status(400).send(`Webhook Error ${err.message}`);
   }
+
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
